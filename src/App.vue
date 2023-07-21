1159bf2ac179ab776b2e7188e74c0da27bf7c1c5
Darina Kašprišinová <darina.kasprisinova@gmail.com>
2023-07-21 15:34:49 +0200

    modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   src/App.vue 	new file:   src/assets/tailwind.css 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/NoteForm.vue 	modified:   src/main.js 	new file:   tailwind.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+  <div :class="{ light: darkMode }">
+    
+      <div class="dark-mode-switch" >
+        <label class="switch">
+          <input type="checkbox" @change="toggleDarkMode" />
+          <span class="slider round"></span>
+        </label>
+      </div>
+
+      <h1>Notebook</h1>
+      <NoteForm />
+   
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NoteForm from "./components/NoteForm.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { NoteForm },
+  data() {
+    return {
+      darkMode: false,
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +39,104 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #d7d4d4;
+  margin-top: 50px;
+}
+body {
+  margin: 0;
+  background: #2c3e50;
+}
+
+body.ligh {
+  margin: 0;
+  background: white;
+}
+
+/* Dark mode css styles */
+
+.dark-mode-switch {
+  position: fixed;
+  top: 10px;
+  right: 10px;
+  z-index: 9999;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 24px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  border-radius: 24px;
+  transition: 0.4s;
+}
+
+.slider.round {
+  background-color: #000;
+}
+
+.slider.round::before {
+  position: absolute;
+  content: "";
+  height: 18px;
+  width: 18px;
+  left: 3px;
+  bottom: 3px;
+  background-color: white;
+  border-radius: 50%;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+input:checked + .slider.round {
+  background-color: rgba(255, 255, 255, 0.775);
+}
+
+input:checked + .slider::before {
+  transform: translateX(16px);
+}
+
+input:checked + .slider.round::before {
+  transform: translateX(20px);
+}
+
+.light {
+  background: #d7d4d4;
+  color: rgb(17, 17, 17);
+}
+
+.light p {
+  color: rgb(70, 54, 54);
+}
+
+.light #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ff0000;
+  margin-top: 50px;
+}
+.light body {
+  margin: 0;
+  background: crimson;
 }
 </style>
---
+++ src/components/NoteForm.vue
@@ -0,0 +1,178 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+      <div class="input-container">
+        <label class="required">Note:</label>
+        <input v-model="note" required />
+        
+        <label class="required">Date:</label>
+        <input type="date" v-model="date" required />
+      </div>
+      <div v-if="noteError" class="error">{{ noteError }}</div>
+      <div class="submit">
+        <button>Add note</button>
+      </div>
+    </form>
+  
+    <h2>Your notes</h2>
+  
+    <div>
+      <ul>
+        <li v-for="not in notes" :key="not.id">
+            <h4>{{ not.note }}</h4>
+            <p>{{ not.date }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { handleError } from "vue";
+  
+  export default {
+    data() {
+      return {
+        note: "",
+        notes: [],
+        noteError: '',
+        date: this.formattedDate(),
+      };
+    },
+    computed: {
+      similarNotes() {
+        if (this.note && this.date) {
+          return this.notes.filter(
+            (item) => this.similarity(this.note, item.note) >= 80 && item.date === this.formatDate(this.date)
+          );
+        }
+        return [];
+      }
+  
+    },
+    methods: {
+      formattedDate() {
+        const currentDate = new Date();
+        const day = currentDate.getDate().toString().padStart(2, "0");
+        const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+        const year = currentDate.getFullYear();
+        const formattedDate = `${year}-${month}-${day}`;
+        return formattedDate;
+      },
+      formatDate(date) {
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        return new Date(date).toLocaleDateString("sk-SK", options);
+      },
+      similarity(a, b) {
+        const setA = new Set(a.toLowerCase());
+        const setB = new Set(b.toLowerCase());
+        const intersection = new Set([...setA].filter((x) => setB.has(x)));
+        return (intersection.size / setA.size) * 100;
+      },
+      handleSubmit() {
+        if (this.similarNotes.length > 0) {
+          const similarNotesText = this.similarNotes.map((item) => item.note).join(', ');
+          this.noteError = (`Poznámka s podobným textom a dátumom už existuje: ${similarNotesText}`);
+        } else {
+          this.notes.push({
+          id: Date.now(),
+          note: this.note,
+          date: this.formatDate(this.date),
+        }
+        );
+        this.note = "";
+        }   
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  form {
+    max-width: 700px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 50px;
+    border-radius: 10px;
+  }
+  
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 15px 15px 0;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  
+  .input-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  
+  input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  button {
+    background: #444;
+    border: 0;
+    font-size: 15px;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+  }
+  button:hover {
+    background: #222;
+  }
+  .submit {
+    text-align: center;
+  }
+  .required::after {
+    content: "*";
+    color: red;
+    margin-left: 2px;
+  }
+  
+  p,
+  h3,
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    max-width: 700px;
+    list-style-type: none;
+    background: #fff;
+    color: rgb(77, 80, 94);
+    margin: 20px auto;
+    padding: 10px 20px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  li.fav {
+    background: #ff9ed2;
+    color: white;
+  }
+  .box {
+    padding: 100px 0;
+    width: 400px;
+    text-align: center;
+    background: #ddd;
+    margin: 20px;
+    display: inline-block;
+  }
+  .error {
+    color: crimson;
+  }
+  </style>
+  
